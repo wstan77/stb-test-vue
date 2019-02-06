3cfec6a2d41f7f5e0fec4645e1de94717a7eee8d
stan0990 <walter_07_8@hotmail.com>
2019-02-06 00:12:37 -0600

    adding form

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,49 +1,141 @@
 <template>
-  <div class="hello">
-    <h1>User list</h1>
-    <p>
-      Simple system done with VueJS for rendering and making CRUD operations with Users got from JSONPlaceholder API.
-    </p>  
-    <a href="#!" class="btn-add-user">+</a>
-    <table class="table" v-if="usersArray.length > 0">
-      <thead class="thead-header">
-        <tr>
-          <th></th>
-          <th>User</th>
-          <th>Website</th>
-          <th>Company</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody> 
-          <tr class="trBody" v-for="user in usersArray" v-bind:key="user.id">
-            <td><p class="letterImage">L</p></td>
-            <td>
-              <h4 class="title text">{{user.name}}</h4>
-              <p class="text text-data"><a class="at-email">@</a> {{user.email}}</p>
-              <p class="text text-data"><img src="../assets/images/marker.png" alt="Marker" class="marker-img"> Kulas Light, Gwenborough</p>
-            </td>
-            <td><a class="text-link" v-bind:href="'//' + user.website" target="blank">{{user.website}}</a></td>
-            <td>
-              <div class="well">
-                <p class="text text-center text-companyName">{{user.company.name}}</p>
-                <p class="text text-center text-slogan">"{{user.company.catchPhrase && user.company.catchPhrase}}"</p>
-              </div>
-            </td>
-            <td>
-              <div class="center">
-                <a href="" class="btn">Editar</a>
-                <a href="#!" class="btn" v-on:click="deleteUser(user.id)">Eliminar</a>
-              </div>
-            </td>
-          </tr> 
-      </tbody>
-    </table>
-    <div v-if="usersArray.length == 0">
-      <p class="notFound">Users not found in this application</p>
+  <div>
+
+
+
+
+    <div class="userList" v-if="showForm==false">
+      <h1>User list</h1>
+      <p>
+        Simple system done with VueJS for rendering and making CRUD operations with Users got from JSONPlaceholder API.
+      </p>  
+      <a href="#!" class="btn-add-user" v-on:click="showForm=true">+</a>
+      <table class="table" v-if="usersArray.length > 0">
+        <thead class="thead-header">
+          <tr>
+            <th></th>
+            <th>User</th>
+            <th>Website</th>
+            <th>Company</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody> 
+            <tr class="trBody" v-for="user in usersArray" v-bind:key="user.id">
+              <td><p class="letterImage">{{user.name[0]}}</p></td>
+              <td>
+                <h4 class="title text">{{user.name}}</h4>
+                <p class="text text-data"><a class="at-email">@</a> {{user.email}}</p>
+                <p class="text text-data"><img src="../assets/images/marker.png" alt="Marker" class="marker-img"> Kulas Light, Gwenborough</p>
+              </td>
+              <td><a class="text-link" v-bind:href="'//' + user.website" target="blank">{{user.website}}</a></td>
+              <td>
+                <div class="well">
+                  <p class="text text-center text-companyName">{{user.company.name}}</p>
+                  <p class="text text-center text-slogan">"{{user.company.catchPhrase && user.company.catchPhrase}}"</p>
+                </div>
+              </td>
+              <td>
+                <div class="center">
+                  <a href="" class="btn">Editar</a>
+                  <a href="#!" class="btn" v-on:click="deleteUser(user.id)">Eliminar</a>
+                </div>
+              </td>
+            </tr> 
+        </tbody>
+      </table>
+      <div v-if="usersArray.length == 0">
+        <p class="notFound">Users not found in this application</p>
+      </div>
+    </div>
+
+
+
+
+    <div class="userForm" v-if="showForm==true">
+      <h1>New User</h1>
+      <p>
+        You can create a new User with his personal information right here!.
+      </p>
+
+        <p v-if="errors.length">
+          <b>Please correct the following error(s):</b>
+          <ul>
+            <li v-for="error in errors" v-bind:key="error.id">{{ error }}</li>
+          </ul>
+        </p>
+      <form 
+        @submit="createUser" 
+        class="userFormFor"
+        method="post"> 
+
+        <label for="userName">User name</label>
+        <br />
+        <input
+          id="userName"
+          v-model="userName"
+          type="text"
+          name="userName"
+          class="input"
+        > 
+        <br />
+        
+        <label for="userEmail">Email </label>
+        <br />
+        <input
+          id="userEmail"
+          v-model="userEmail"
+          type="text"
+          name="userEmail"
+          class="input"
+        >
+        <br />
+        
+        <label for="userWebsite">Website</label>
+        <br />
+        <input
+          id="userWebsite"
+          v-model="userWebsite"
+          type="text"
+          name="userWebsite"
+          class="input"
+        >
+        <br />
+        
+        <label for="company.companyName">Company Name</label>
+        <br />
+        <input
+          id="company.companyName"
+          v-model="company.companyName"
+          type="text"
+          name="company.companyName"
+          class="input"
+        >
+        <br />
+        
+        <label for="company.companyCatchPhrase">Company Catch Phrase</label>
+        <br />
+        <input
+          id="company.companyCatchPhrase"
+          v-model="company.companyCatchPhrase"
+          type="text"
+          name="company.companyCatchPhrase"
+          class="input"
+        > 
+        <br />
+        
+        <input
+          type="submit"
+          value="Create user"
+          class="btn-submit-user"
+        > 
+        <a href="#!" v-on:click="showForm=false" class="btn-cancel-user">Cancel</a>
+      </form>
     </div>
   </div>
+  
 </template> 
+
 
 <script>
 import "../assets/styles/usersComponent.css";
@@ -51,9 +143,6 @@ import axios from 'axios';
 
 export default {
   name: 'Users',
-  props: {
-    connection: String
-  },
   mounted(){
     axios
       .get('https://jsonplaceholder.typicode.com/users')
@@ -68,7 +157,16 @@ export default {
   },
   data(){
     return{
-      usersArray: []
+      usersArray: [],
+      showForm: false,
+      errors: [],
+      userName: null,
+      userEmail: null,
+      userWebsite: null,
+      company: {
+        companyName: null,
+        companyCatchPhrase: null
+      }
     }
   },
   methods:{
@@ -76,8 +174,33 @@ export default {
       const usersList = JSON.parse(localStorage.getItem("USERS"));
       const userIndex = usersList.findIndex(r=>r.id == id);
       const newUserList = usersList.filter(r=>r.id !== usersList[userIndex].id);
+
       localStorage.setItem("USERS", JSON.stringify(newUserList))
       this.usersArray = JSON.parse(localStorage.getItem("USERS"));
+    },
+    createUser: function (e) {
+      e.preventDefault();
+      if (this.userName && this.userEmail) { 
+        const usersList = JSON.parse(localStorage.getItem("USERS"));
+        usersList.unshift({
+          id: this.usersArray.length + 1,
+          name: this.userName,
+          email: this.userEmail,
+          website: this.userWebsite,
+          company: {
+            name: this.company.companyName,
+            catchPhrase: this.company.companyCatchPhrase
+          }
+        });
+
+        localStorage.setItem("USERS", JSON.stringify(usersList))
+        this.usersArray = JSON.parse(localStorage.getItem("USERS"));
+        this.showForm=false;
+      }else{
+        if (!this.userName) {
+          this.errors.push('Name required.');
+        }
+      }
     }
   }
 }
